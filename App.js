6d305deb189b1254cc78d410b40b795576e0bd93
Michael Krasnov <mihalKrasnov@gmail.com>
2020-02-25 12:08:15 -0500

    Added UI and tests

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={styles.label}
+        testID={'counter'}
+      >
+        You clicked the button {counter} times.
+      </Text>
+      <Button
+        testID={'button'}
+        onPress={() => setCounter(counter + 1)}
+        title={'Press me'}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  label: {
+    marginBottom: 16
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
